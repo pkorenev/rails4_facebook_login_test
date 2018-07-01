ded6de6a8fad430e3520400431b2fbcd4e89a741
pkorenev <p.korenev@voroninstudio.eu>
2018-07-01 19:06:53 +0300

    auth

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,6 +1,6 @@
 Koala.configure do |config|
-  config.app_id =
-  config.app_secret = Rails.application.secrets.facebook_app_secret
+  config.app_id = ENV['FACEBOOK_APP_ID']
+  config.app_secret = ENV['FACEBOOK_APP_SECRET']
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
